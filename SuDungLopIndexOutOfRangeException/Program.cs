58932e54557ac1a727252f7a443b429b8a4ef3bd
NguyenSanhHuyVu <nshvu.19it2@vku.udn.vn>
2023-09-15 13:44:58 +0700

    BTbuoi11

---
+++ SuDungLopIndexOutOfRangeException/Program.cs
@@ -0,0 +1,19 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Clear();
+        try
+        {
+            List<char> chars = new List<char>();
+
+            chars.InsertRange(0, new Char[] { 'a', 'b', 'c', 'd', 'e', 'f' });
+
+            int value = chars[6];
+        }
+        catch (System.Exception e)
+        {
+            Console.WriteLine(e);
+        }
+    }
+}
