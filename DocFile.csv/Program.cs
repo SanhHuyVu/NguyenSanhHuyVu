104e806b35d34d319facebd94e423a2389e1dddc
NguyenSanhHuyVu <nshvu.19it2@vku.udn.vn>
2023-09-17 22:40:48 +0700

    BTbuoi12

---
+++ DocFile.csv/Program.cs
@@ -0,0 +1,18 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        string path = "nations.csv";
+
+        StreamReader reader = new StreamReader(path);
+
+        string line = "";
+
+        while (!reader.EndOfStream)
+        {
+            line = reader.ReadLine();
+            string[] temp = line.Replace("\"", "").Split(",");
+            Console.WriteLine(temp[5]);
+        }
+    }
+}
---
+++ TinhTongCacSoTrongFileText/Program.cs
@@ -0,0 +1,11 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        string path = "numbers.txt";
+        ReadTextFileExample reader = new ReadTextFileExample();
+
+        Console.Clear();
+        reader.ReadTextFile(path);
+    }
+}
